ec7d4c52a1ac04424183ca26e23272569c89e897
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2021-01-10 11:51:58 +0100

    Create Image.java

---
+++ src/Image.java
@@ -0,0 +1,62 @@
+public class Image {
+
+    private String lien;
+
+    public Image(String lien)
+    {
+        this.lien = lien;
+    }
+
+    private Image binarisation()
+    {
+        return null;
+    }
+
+    public Image seuillage(int seuil)
+    {
+        return null;
+    }
+
+    public Image addition(Image img)
+    {
+        return null;
+    }
+
+    public Image soustraction(Image img)
+    {
+        return null;
+    }
+
+    public Image erosion()
+    {
+        return null;
+    }
+
+    public Image dilatation()
+    {
+        return null;
+    }
+
+    public Image ouverture()
+    {
+        return null;
+    }
+
+    public Image fermeture()
+    {
+        return null;
+    }
+
+    public Image amincissement()
+    {
+        return null;
+    }
+
+    public Image epaississement()
+    {
+        return null;
+    }
+
+
+
+}
