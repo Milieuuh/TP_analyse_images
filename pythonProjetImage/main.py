ab46856766889983ecc483fd74bd27451dbf29be
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2021-01-16 18:54:57 +0100

    dfgh

--- pythonProjetImage/main.py
+++ pythonProjetImage/main.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-
 def seuillage(seuil, img):
     print("seuillage")
     imageSeuil = img.copy()
@@ -28,9 +27,9 @@ def erosion(img, elementStructurant):
             somme = 0
             for k in range(0 - widthElt//2, widthElt - widthElt//2):
                 for l in range(0 - heightElt //2, heightElt -heightElt //2):
-                    somme = somme +img[i+k,j-l] * elementStructurant[k+widthElt//2,l+heightElt//2]
+                    somme = somme + img[i+k,j+l] * elementStructurant[k+widthElt//2,l+heightElt//2]
 
-            if somme == 1275:
+            if somme >= 1275:
                 imageErosion[i,j] = 255
             else:
                 imageErosion[i,j] = 0
@@ -52,7 +51,7 @@ def dilatation(img, elementStructurant):
             somme = 0
             for k in range(0 - widthElt//2, widthElt - widthElt//2):
                 for l in range(0 - heightElt //2, heightElt -heightElt //2):
-                    somme = somme +img[i+k,j-l] * elementStructurant[k+widthElt//2,l+heightElt//2]
+                    somme = somme +img[i+k,j+l] * elementStructurant[k+widthElt//2,l+heightElt//2]
 
             if somme == 0:
                 imageDilatation[i,j] = 0
@@ -68,12 +67,13 @@ def addition(img, img2):
 
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            pixel = img[i, j] + img2[i, j]
-
+            val1 = int(img[i, j])
+            val2 = int(img2[i, j])
+            pixel = val1 + val2
             if pixel>255:
-                pixel=255
-
-            imgAddition[i,j]=pixel
+                imgAddition[i,j]=255
+            else:
+                imgAddition[i,j]=pixel
 
     return imgAddition
 
@@ -85,12 +85,13 @@ def soustraction(img, img2):
 
     for i in range(img.shape[0]):
         for j in range(img.shape[1]):
-            pixel = img[i, j] - img2[i, j]
-
+            val1 = int(img[i, j])
+            val2 = int(img2[i, j])
+            pixel = val1 - val2
             if pixel < 0:
-                pixel = 0
-
-            imgSoustraction[i, j] = pixel
+                imgSoustraction[i, j] = 0
+            else:
+                imgSoustraction[i, j] = pixel
 
     return imgSoustraction
 
@@ -110,34 +111,109 @@ def fermeture(img, elementStructurant):
 
     return result
 
-def squeletisationLantuejoul(img,elementStructurant,nbIteration):
+def squeletisationLantuejoul(img,elementStructurant2,nbIteration):
     print("Lantuejoul")
-    imgAdd=img.copy
-    for nb in range(1,nbIteration):
-        imgErode=erosion(imgSeuil, nb*elementStructurant)
-        imgOuvertureErode=ouverture(imgErode,elementStructurant)
-        imgSous=soustraction(imgErode,imgOuvertureErode)
-        if nb!=1:
+    imgAdd=img.copy()
+    for nb in range(0,nbIteration):
+        imgErode=erosion(img, nb*elementStructurant2)
+        imgOuvertureErode=ouverture(imgErode, elementStructurant2)
+        imgSous=soustraction(imgErode, imgOuvertureErode)
+        if nb>0:
             imgAdd=addition(imgAdd, imgSous)
         else:
             imgAdd=imgSous
     return imgAdd
 
+def amincissement(img, elementStructurant):
+    imgAmincie = img.copy()
+
+    # largeur et longueur de l'élément structurant
+    widthElt = elementStructurant.shape[0]
+    heightElt = elementStructurant.shape[1]
+
+    for i in range(widthElt // 2, img.shape[0] - widthElt // 2):
+        for j in range(heightElt // 2, img.shape[1] - heightElt // 2):
+            bool = True
+            for k in range(0 - widthElt // 2, widthElt - widthElt // 2):
+                for l in range(0 - heightElt // 2, heightElt - heightElt // 2):
+                    var = elementStructurant[k+1, l+1]
+                    if(var==1):
+                        var =255
+                    if img[i+k,j+l]!=var and elementStructurant[k+1,l+1]!=2:
+                       bool = False
+            if bool == False:
+                imgAmincie[i,j]=0
+            else:
+                imgAmincie[i,j]=255
+
+    return imgAmincie
+
+def epaississement(img, elementStructurant):
+    imgEpaississement = img.copy()
+
+    # largeur et longueur de l'élément structurant
+    widthElt = elementStructurant.shape[0]
+    heightElt = elementStructurant.shape[1]
+
+    for i in range(widthElt // 2, img.shape[0] - widthElt // 2):
+        for j in range(heightElt // 2, img.shape[1] - heightElt // 2):
+            bool = True
+            for k in range(0 - widthElt // 2, widthElt - widthElt // 2):
+                for l in range(0 - heightElt // 2, heightElt - heightElt // 2):
+                    var = elementStructurant[k+1, l+ 1]
+                    if(var ==1):
+                        var =255
+                    print(var)
+                    if img[i+k,j+l]==var and elementStructurant[k+1 ,l+1]!=2:
+                       bool = False
+            if bool == False:
+                imgEpaississement[i,j]=0
+            else:
+                imgEpaississement[i,j]=255
+
+    return imgEpaississement
+
+def differenceImage(img1, img2):
+    bool = True
+    for i in range(img1.shape[0]):
+        for j in range(img1.shape[1]):
+            if img1[i,j] != img2[i,j]:
+                bool = False
+    return bool
+
+
+def squelettisationParAmincissement(img, elementStructurant):
+    imgSquelette = img.copy()
+    nb=0
+    while True:
+        imgAvant = imgSquelette
+        if nb==0:
+            imgSquelette = amincissement(img, elementStructurant)
+        else:
+            imgSquelette = amincissement(imgSquelette, elementStructurant)
+        nb=nb+1
+        if differenceImage(imgSquelette, imgAvant):
+            break
+
+    return imgSquelette
+
+
 ######################MAIN
-img = cv.imread('fruits.jpg')
-img2 = cv.imread('fruits2.jpg')
-#img = cv.imread('croix.png')
-#img2 = cv.imread('croix2.png')
+# img = cv.imread('fruits.jpg')
+# img2 = cv.imread('fruits2.jpg')
+img = cv.imread('croix_61.png')
+img2 = cv.imread('croix2.png')
 #img = cv.imread('Rect.png')
 imgGray= cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 #imgGray2= cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
 
+elementStructurantAE = np.array([[1, 2, 1], [1, 1, 2], [2, 1, 2]])
 elementStructurant = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])
 
 
 imgSeuil = seuillage(120, imgGray)
 
-imgErodee = erosion(imgSeuil, elementStructurant)
+#imgErodee = erosion(imgSeuil, elementStructurant)
 #
 # imgDilatee = dilatation(imgSeuil, elementStructurant)
 #
@@ -149,11 +225,14 @@ imgErodee = erosion(imgSeuil, elementStructurant)
 #imgAdd = addition(imgGray, imgGray2)
 #imgSous = soustraction(imgGray, imgGray2)
 
-# imgL = squeletisationLantuejoul(imgGray,elementStructurant,2)
+#imgL = squeletisationLantuejoul(imgSeuil,elementStructurant,10)
+#imgL = epaississement(imgSeuil,elementStructurantAE)
+#imgL = amincissement(imgSeuil,elementStructurantAE)
+imgL = squelettisationParAmincissement(imgSeuil, elementStructurantAE)
 #
-# plt.subplot(3, 3, 1)
-# plt.title("Image d'origine")
-# plt.imshow(imgGray, cmap='gray')
+plt.subplot(1, 2, 1)
+plt.title("Image d'origine")
+plt.imshow(imgSeuil, cmap='gray')
 #
 # plt.subplot(3, 3, 2)
 # plt.title("Image seuillage")
@@ -183,9 +262,9 @@ imgErodee = erosion(imgSeuil, elementStructurant)
 # plt.title("Image soustraction")
 # plt.imshow(imgSous,cmap='gray')
 
-# plt.subplot(3, 3, 9)
-# plt.title("Image Lantuejoul")
-# plt.imshow(imgL,cmap='gray')
+plt.subplot(1, 2, 2)
+plt.title("Image Lantuejoul")
+plt.imshow(imgL, cmap='gray')
 
 plt.show()
 
