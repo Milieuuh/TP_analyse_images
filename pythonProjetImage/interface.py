c1793aa1d82e80d36e96d0c7bf245d22c4aaa5f8
BrightHammer21 <lucieboucquemont@hotmail.fr>
2021-01-17 16:56:11 +0100

    Update interface.py

--- pythonProjetImage/interface.py
+++ pythonProjetImage/interface.py
@@ -153,9 +153,8 @@ def epaississement(lablResult, path1):
     lablResult.place(x=0, y=0)
 
 def squelettisation(lablResult, path1, nb):
-    #elementStructurant = np.array([[1, 2, 1], [1, 1, 2], [2, 1, 2]])
+    elementStructurant = np.array([[0, 1, 0], [1, 1, 1], [0, 1, 0]])
     #elementStructurant = np.array([[0, 0, 0], [2, 1, 2], [1, 1, 1]])
-    elementStructurant = np.array([[2, 0, 0], [1, 1, 0], [2, 1, 2]])
     img = cv.imread(path1)
     imgGray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     imgSeuil = main.seuillage(120, imgGray)
